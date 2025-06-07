02fc633cc012904ccb95bc39f4f6bd0be7a8b223
Steven <bracke.s85@gmail.com>
2025-06-07 16:14:55 +0200

    game.js update

--- js/game.js
+++ js/game.js
@@ -5,8 +5,9 @@ let character = new Image();
 function init(){
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext('2d')
+ 
+ 
     character.src = '../img/2_character_pepe/2_walk/W-21.png'
 
-    
     ctx.drawImage(character,20,20,50,150)
 }

f658621e1d639a18eeed88f9f0776b49d32b5819
Steven <bracke.s85@gmail.com>
2025-07-09 14:26:30 +0200

    adding bottle & coin to level

---
+++ levels/level1 copy.js
@@ -0,0 +1,31 @@
+const level1 = new Level(
+    [
+        new Chicken(),
+        new Chicken(),
+        new Chicken()
+    ],
+    [
+        new Endboss()
+    ],
+    [
+        new Cloud()
+    ],
+    [
+        new BackgroundObject('img/5_background/layers/air.png', -100, 0),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', -100, 0),
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', -100, 0),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', -100, 0),
+        new BackgroundObject('img/5_background/layers/air.png', 620, 0),
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 620, 0),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 620, 0),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 620, 0),
+        new BackgroundObject('img/5_background/layers/air.png', 720 * 2 - 100, 0),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 720 * 2 - 100, 0),
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 720 * 2 - 100, 0),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 720 * 2 - 100, 0),
+        new BackgroundObject('img/5_background/layers/air.png', 720 * 3 - 100, 0),
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 720 * 3 - 100, 0),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 720 * 3 - 100, 0),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 720 * 3 - 100, 0)
+    ]
+);

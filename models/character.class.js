79bd5ae654d0f1190a68f26cd47e0266ff3dc992
Steven <bracke.s85@gmail.com>
2025-07-16 20:42:28 +0200

    reading local storage

--- models/character.class.js
+++ models/character.class.js
@@ -80,6 +80,12 @@ class Character extends MovableObject {  // durch 'extends' alle Variablen und F
         this.loadImages(this.images_dead)
         this.applyGravity()
         this.animate();
+        this.sounds = [
+            this.walking_sound,
+            this.hurt_sound,
+            this.die_sound,
+            this.jump_sound
+        ];
         this.walking_sound = new Audio('audio/footstep2.mp3');
         this.hurt_sound = new Audio('audio/ouchie.mp3');
         this.die_sound = new Audio('audio/die-sound.mp3');

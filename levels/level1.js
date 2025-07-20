6339579873352411a7e9301f3d69072010448a62
Steven <bracke.s85@gmail.com>
2025-07-20 22:22:10 +0200

    adding JSDOC to level1

--- levels/level1.js
+++ levels/level1.js
@@ -3,8 +3,8 @@ let level1;
 function createLevel() {
     return new Level(
         [
-            ...createElements('chicken', 3,300,50),
-            ...createElements('chickensmall', 3,300,50),
+            ...createElements('chicken', 3, 300, 50),
+            ...createElements('chickensmall', 3, 300, 50),
         ],
         [
             new Endboss()
@@ -39,56 +39,77 @@ function createLevel() {
         ]
         ,
         [
-            ...createElements('coins', 5,150,50)
+            ...createElements('coins', 5, 150, 50)
         ],
-        [ 
-            ...createElements('bottles',5,200,75)
+        [
+            ...createElements('bottles', 5, 200, 75)
         ]
     );
 }
 
-function createElements(type, count,startX,minDistance, options = {}) {
-    const {
-        range = 1000
-    } = options;
-
+/**
+ * Creates game elements with minimum distance between them
+ * 
+ * @param {string} type - Element type (chicken, chickensmall, coins, bottles)
+ * @param {number} count - Number of elements to create
+ * @param {number} startX - Starting X position
+ * @param {number} minDistance - Minimum distance between elements
+ * @param {Object} options - Additional options
+ * @param {number} options.range - Range for random X positioning
+ * @returns {Array} Array of created elements
+ */
+function createElements(type, count, startX, minDistance, options = {}) {
+    const { range = 1000 } = options;
     const elements = [];
 
     while (elements.length < count) {
-        let valid = true;
-        let randomX = startX + Math.random() * range;
-
-        for (let el of elements) {
-            if (Math.abs(el.x - randomX) < minDistance) {
-                valid = false;
-                break;
-            }
-        }
-
-        if (valid) {
-            let element;
-            switch (type) {
-                case 'chicken':
-                    element = new Chicken();
-                    break;
-                case 'chickensmall':
-                    element = new Chickensmall();
-                    break;
-                case 'coins':
-                    element = new Coins();
-                    break;
-                case 'bottles':
-                    element = new Bottles();
-                    break;
-                // weitere Typen nach Bedarf
-                default:
-                    throw new Error(`Unbekannter Elementtyp: ${type}`);
-            }
-            if((this instanceof Coins)){element.x = randomX};
+        const randomX = generateValidPosition(elements, startX, range, minDistance);
+        if (randomX !== null) {
+            const element = createElement(type);
             element.x = randomX;
             elements.push(element);
         }
     }
-
     return elements;
+}
+
+/**
+ * Generates valid X position with minimum distance check
+ * 
+ * @param {Array} elements - Existing elements
+ * @param {number} startX - Starting X position
+ * @param {number} range - Position range
+ * @param {number} minDistance - Minimum distance required
+ * @returns {number|null} Valid position or null if not found
+ */
+function generateValidPosition(elements, startX, range, minDistance) {
+    const randomX = startX + Math.random() * range;
+
+    for (let el of elements) {
+        if (Math.abs(el.x - randomX) < minDistance) {
+            return null;
+        }
+    }
+    return randomX;
+}
+
+/**
+ * Creates element instance based on type
+ * 
+ * @param {string} type - Element type
+ * @returns {Object} Created element instance
+ */
+function createElement(type) {
+    const elementMap = {
+        'chicken': () => new Chicken(),
+        'chickensmall': () => new Chickensmall(),
+        'coins': () => new Coins(),
+        'bottles': () => new Bottles()
+    };
+
+    if (!elementMap[type]) {
+        throw new Error(`Unknown element type: ${type}`);
+    }
+
+    return elementMap[type]();
 }

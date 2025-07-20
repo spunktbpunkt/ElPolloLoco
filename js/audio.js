e400f3e697ba45a1c23762f5ef8ff923b6edc42d
Steven <bracke.s85@gmail.com>
2025-07-20 22:17:32 +0200

    cleaning up

--- js/audio.js
+++
@@ -1,8 +0,0 @@
-let rightSound = new Audio('sound/right.mp3');
-let wrongSound = new Audio('sound/wrong.mp3');
-let winnerSound = new Audio('sound/winner.mp3');
-let applausSound = new Audio('sound/applaus.mp3');
-
-function jumpSound(){
-    rightSound.play();
-}

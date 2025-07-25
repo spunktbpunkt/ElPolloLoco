dc26813200598be7a5198af94606f9126d974b92
Steven <bracke.s85@gmail.com>
2025-07-25 21:16:50 +0200

    more background

--- levels/level1.js
+++ levels/level1.js
@@ -21,6 +21,8 @@
                 new BackgroundObject('img/5_background/layers/air.png', 720 * 2, 0),
                 new BackgroundObject('img/5_background/layers/air.png', 720 * 3, 0),
                 new BackgroundObject('img/5_background/layers/air.png', 720 * 4, 0),
+                new BackgroundObject('img/5_background/layers/air.png', 720 * 5, 0),
+                new BackgroundObject('img/5_background/layers/air.png', 720 * 6, 0),
 
                 new BackgroundObject('img/5_background/layers/3_third_layer/2.png', -720, 0),
                 new BackgroundObject('img/5_background/layers/2_second_layer/2.png', -720, 0),
@@ -44,7 +46,15 @@
 
                 new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 720 * 4, 0),
                 new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 720 * 4, 0),
-                new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 720 * 4, 0)
+                new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 720 * 4, 0),
+
+                new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 720 * 5, 0),
+                new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 720 * 5, 0),
+                new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 720 * 5, 0),
+
+                new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 720 * 6, 0),
+                new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 720 * 6, 0),
+                new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 720 * 6, 0)
 
             ]
             ,

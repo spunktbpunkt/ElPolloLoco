42737f991b0922345e14cb14c36579b18cf43668
Steven <bracke.s85@gmail.com>
2025-07-25 20:07:04 +0200

    shorter character.class.js

--- models/character.class.js
+++ models/character.class.js
@@ -3,412 +3,282 @@ class Character extends MovableObject {
     height = 300;
     width = 160;
     speed = 10;
-    images_walking = [
-        'img/2_character_pepe/2_walk/W-21.png',
-        'img/2_character_pepe/2_walk/W-22.png',
-        'img/2_character_pepe/2_walk/W-23.png',
-        'img/2_character_pepe/2_walk/W-24.png',
-        'img/2_character_pepe/2_walk/W-25.png',
-        'img/2_character_pepe/2_walk/W-26.png'
-    ]
 
-    images_dead = [
-        'img/2_character_pepe/5_dead/D-51.png',
-        'img/2_character_pepe/5_dead/D-52.png',
-        'img/2_character_pepe/5_dead/D-53.png',
-        'img/2_character_pepe/5_dead/D-54.png',
-        'img/2_character_pepe/5_dead/D-55.png',
-        'img/2_character_pepe/5_dead/D-56.png',
-        'img/2_character_pepe/5_dead/D-57.png'
-    ]
-    images_hurt = [
-        'img/2_character_pepe/4_hurt/H-41.png',
-        'img/2_character_pepe/4_hurt/H-42.png',
-        'img/2_character_pepe/4_hurt/H-43.png',
-    ]
+    animations = {
+        walking: [
+            'img/2_character_pepe/2_walk/W-21.png',
+            'img/2_character_pepe/2_walk/W-22.png',
+            'img/2_character_pepe/2_walk/W-23.png',
+            'img/2_character_pepe/2_walk/W-24.png',
+            'img/2_character_pepe/2_walk/W-25.png',
+            'img/2_character_pepe/2_walk/W-26.png'
+        ],
+        dead: [
+            'img/2_character_pepe/5_dead/D-51.png',
+            'img/2_character_pepe/5_dead/D-52.png',
+            'img/2_character_pepe/5_dead/D-53.png',
+            'img/2_character_pepe/5_dead/D-54.png',
+            'img/2_character_pepe/5_dead/D-55.png',
+            'img/2_character_pepe/5_dead/D-56.png',
+            'img/2_character_pepe/5_dead/D-57.png'
+        ],
+        hurt: [
+            'img/2_character_pepe/4_hurt/H-41.png',
+            'img/2_character_pepe/4_hurt/H-42.png',
+            'img/2_character_pepe/4_hurt/H-43.png',
+        ],
+        idle: [
+            'img/2_character_pepe/1_idle/idle/I-1.png',
+            'img/2_character_pepe/1_idle/idle/I-2.png',
+            'img/2_character_pepe/1_idle/idle/I-3.png',
+            'img/2_character_pepe/1_idle/idle/I-4.png',
+            'img/2_character_pepe/1_idle/idle/I-5.png',
+            'img/2_character_pepe/1_idle/idle/I-6.png',
+            'img/2_character_pepe/1_idle/idle/I-7.png',
+            'img/2_character_pepe/1_idle/idle/I-8.png',
+            'img/2_character_pepe/1_idle/idle/I-9.png',
+            'img/2_character_pepe/1_idle/idle/I-10.png'
+        ],
+        longIdle: [
+            'img/2_character_pepe/1_idle/long_idle/I-11.png',
+            'img/2_character_pepe/1_idle/long_idle/I-12.png',
+            'img/2_character_pepe/1_idle/long_idle/I-13.png',
+            'img/2_character_pepe/1_idle/long_idle/I-14.png',
+            'img/2_character_pepe/1_idle/long_idle/I-15.png',
+            'img/2_character_pepe/1_idle/long_idle/I-16.png',
+            'img/2_character_pepe/1_idle/long_idle/I-17.png',
+            'img/2_character_pepe/1_idle/long_idle/I-18.png',
+            'img/2_character_pepe/1_idle/long_idle/I-19.png',
+            'img/2_character_pepe/1_idle/long_idle/I-20.png'
+        ],
+        jumping: [
+            'img/2_character_pepe/3_jump/J-31.png',
+            'img/2_character_pepe/3_jump/J-32.png',
+            'img/2_character_pepe/3_jump/J-33.png',
+            'img/2_character_pepe/3_jump/J-34.png'
+        ],
+        falling: [
+            'img/2_character_pepe/3_jump/J-35.png',
+            'img/2_character_pepe/3_jump/J-36.png',
+            'img/2_character_pepe/3_jump/J-37.png',
+            'img/2_character_pepe/3_jump/J-38.png',
+            'img/2_character_pepe/3_jump/J-39.png'
+        ]
+    };
 
-    images_idle = [
-        'img/2_character_pepe/1_idle/idle/I-1.png',
-        'img/2_character_pepe/1_idle/idle/I-2.png',
-        'img/2_character_pepe/1_idle/idle/I-3.png',
-        'img/2_character_pepe/1_idle/idle/I-4.png',
-        'img/2_character_pepe/1_idle/idle/I-5.png',
-        'img/2_character_pepe/1_idle/idle/I-6.png',
-        'img/2_character_pepe/1_idle/idle/I-7.png',
-        'img/2_character_pepe/1_idle/idle/I-8.png',
-        'img/2_character_pepe/1_idle/idle/I-9.png',
-        'img/2_character_pepe/1_idle/idle/I-10.png'
-    ]
-    images_longidle = [
-        'img/2_character_pepe/1_idle/long_idle/I-11.png',
-        'img/2_character_pepe/1_idle/long_idle/I-12.png',
-        'img/2_character_pepe/1_idle/long_idle/I-13.png',
-        'img/2_character_pepe/1_idle/long_idle/I-14.png',
-        'img/2_character_pepe/1_idle/long_idle/I-15.png',
-        'img/2_character_pepe/1_idle/long_idle/I-16.png',
-        'img/2_character_pepe/1_idle/long_idle/I-17.png',
-        'img/2_character_pepe/1_idle/long_idle/I-18.png',
-        'img/2_character_pepe/1_idle/long_idle/I-19.png',
-        'img/2_character_pepe/1_idle/long_idle/I-20.png'
-    ]
+    sounds = {
+        walking: { file: 'audio/footstep.mp3', volume: 'soundVolume' },
+        hurt: { file: 'audio/ouchie.mp3', volume: 'soundVolume' },
+        die: { file: 'audio/die-sound.mp3', volume: 'soundVolume' },
+        jump: { file: 'audio/jump-sound.mp3', volume: 'soundVolume' },
+        snoring: { file: 'audio/snorring.wav', volume: 'soundVolume' }
+    };
 
-    images_jumping = [
-        'img/2_character_pepe/3_jump/J-31.png',
-        'img/2_character_pepe/3_jump/J-32.png',
-        'img/2_character_pepe/3_jump/J-33.png',
-        'img/2_character_pepe/3_jump/J-34.png'
-    ];
-
-    images_falling = [
-        'img/2_character_pepe/3_jump/J-35.png',
-        'img/2_character_pepe/3_jump/J-36.png',
-        'img/2_character_pepe/3_jump/J-37.png',
-        'img/2_character_pepe/3_jump/J-38.png',
-        'img/2_character_pepe/3_jump/J-39.png'
-    ];
+    animationConfig = {
+        dead: { type: 'once', sound: 'die', callback: () => setTimeout(() => youLose(), 1000) },
+        hurt: { type: 'loop', sound: 'hurt' },
+        jumping: { type: 'once', interval: 50 },
+        falling: { type: 'once', interval: 50 },
+        walking: { type: 'loop', sound: 'walking' },
+        idle: { type: 'loop', frameSkip: 4 },
+        longIdle: { type: 'loop', frameSkip: 8, sound: 'snoring' }
+    };
 
     world;
-    offset = {
-        left: 30,
-        right: 70,
-        top: 115,
-        bottom: 10
-    }
-    falling = false
+    offset = { left: 30, right: 70, top: 115, bottom: 10 };
+    falling = false;
     jumping = false;
-    jumpInterval;
-    waitInterval;
-    fallInterval;
-    movingInterval;
-    animationInterval;
+    intervals = {};
     lastKeyboardHit = 0;
-
+    frameCounter = 0;
+    currentState = 'idle';
 
     constructor() {
         super();
-        this.loadImage('img/2_character_pepe/2_walk/W-21.png')
-        this.loadImages(this.images_walking)
-        this.loadImages(this.images_idle)
-        this.loadImages(this.images_longidle)
-        this.loadImages(this.images_jumping)
-        this.loadImages(this.images_falling)
-        this.loadImages(this.images_hurt)
-        this.loadImages(this.images_dead)
-        this.applyGravity()
+        this.loadImage(this.animations.walking[0]);
+        this.loadAllAnimations();
+        this.loadAllSounds();
+        this.applyGravity();
         this.animate();
         this.lastKeyboardHit = new Date().getTime();
-        this.walking_sound = new Audio('audio/footstep.mp3');
-        this.hurt_sound = new Audio('audio/ouchie.mp3');
-        this.die_sound = new Audio('audio/die-sound.mp3');
-        this.jump_sound = new Audio('audio/jump-sound.mp3');
-        this.snorring_sound = new Audio('audio/snorring.wav');
-
     }
 
     /**
-    * Starts character animation and movement
-    * 
-    * @returns {void}
-    */
+     * Loads all animation image arrays
+     */
+    loadAllAnimations() {
+        Object.values(this.animations).forEach(images => {
+            this.loadImages(images);
+        });
+    }
+
+    /**
+     * Loads all sound files
+     */
+    loadAllSounds() {
+        Object.entries(this.sounds).forEach(([key, config]) => {
+            this[key + '_sound'] = new Audio(config.file);
+        });
+    }
+
+    /**
+     * Main animation loop
+     */
     animate() {
         if (isPaused) return;
-        this.moving()
-        this.animation();
+        this.startMovement();
+        this.startAnimation();
     }
 
     /**
-     * Handles character movement based on keyboard input
-     * 
-     * @returns {void}
+     * Starts movement interval
      */
-    moving() {
-        if (isPaused) return;
-        this.movingInterval = setInterval(() => {
-            if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
-                this.movingRight()
-            }
-            if (this.world.keyboard.LEFT && this.x > 0) {
-                this.movingLeft()
-            }
-            if ((this.world.keyboard.UP || this.world.keyboard.SPACE) && !this.isAboveGround()) {
-                this.movingJumping()
-            }
+    startMovement() {
+        this.intervals.movement = setInterval(() => {
+            if (isPaused) return;
+            
+            this.handleMovementInput();
             this.world.camera_x = -this.x + 100;
         }, 1000 / 60);
     }
 
     /**
-     * Moves character right with sound
-     * 
-     * @returns {void}
+     * Handles movement input
      */
-    movingRight() {
-        this.otherDirection = false;
-        this.playSound(this.walking_sound, soundVolume);
-        this.moveRight();
+    handleMovementInput() {
+        if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
+            this.moveWithSound('right');
+        }
+        if (this.world.keyboard.LEFT && this.x > 0) {
+            this.moveWithSound('left');
+        }
+        if ((this.world.keyboard.UP || this.world.keyboard.SPACE) && !this.isAboveGround()) {
+            this.jumpWithSound();
+        }
     }
 
     /**
-     * Moves character left with sound
-     * 
-     * @returns {void}
+     * Movement with sound
      */
-    movingLeft() {
-        this.otherDirection = true;
+    moveWithSound(direction) {
+        this.otherDirection = direction === 'left';
         this.playSound(this.walking_sound, soundVolume);
-        this.moveLeft();
+        direction === 'left' ? this.moveLeft() : this.moveRight();
     }
 
     /**
-     * Makes character jump with sound
-     * 
-     * @returns {void}
+     * Jump with sound
      */
-    movingJumping() {
+    jumpWithSound() {
         this.playSound(this.jump_sound, soundVolume);
         this.jump();
     }
 
     /**
-     * Stops all character intervals
-     * 
-     * @returns {void}
+     * Starts animation interval
      */
-    stopAllIntervals() {
-        if (this.movingInterval) {
-            clearInterval(this.movingInterval);
-            this.movingInterval = null;
-        }
-        if (this.animationInterval) {
-            clearInterval(this.animationInterval);
-            this.animationInterval = null;
-        }
-        if (this.jumpInterval) {
-            clearInterval(this.jumpInterval);
-            this.jumpInterval = null;
-        }
-        if (this.fallInterval) {
-            clearInterval(this.fallInterval);
-            this.fallInterval = null;
-        }
-        if (this.waitInterval) {
-            clearInterval(this.waitInterval);
-            this.waitInterval = null;
-        }
-
-        this.stopSnorring();
-    }
-
-    /**
-     * Handles character animation states
-     * 
-     * @returns {void}
-     */
-    animation() {
-        if (isPaused) return;
-        this.currentImageOnce = 0;
-
-        if (!this.frameCounter) this.frameCounter = 0;
-
-        this.animationInterval = setInterval(() => {
+    startAnimation() {
+        this.intervals.animation = setInterval(() => {
+            if (isPaused) return;
+            
             this.frameCounter++;
-            this.handleKeyboardInput();
-            this.handleGameEndCheck();
-            this.selectAnimation();
+            this.updateLastInput();
+            this.updateCurrentState();
+            this.playCurrentAnimation();
         }, 50);
     }
 
     /**
-     * Handles keyboard input tracking
-     * 
-     * @returns {void}
+     * Updates last input time
      */
-    handleKeyboardInput() {
-        if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT || this.world.keyboard.UP || this.world.keyboard.SPACE || this.world.keyboard.D) {
+    updateLastInput() {
+        if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT || 
+            this.world.keyboard.UP || this.world.keyboard.SPACE || this.world.keyboard.D) {
             this.lastKeyboardHit = new Date().getTime();
-            this.stopSnorring();
+            this.stopSnoring();
         }
     }
 
     /**
-     * Checks game end state and stops snoring
-     * 
-     * @returns {void}
+     * Determines current animation state
      */
-    handleGameEndCheck() {
-        if (this.world && this.world.gameEnd) {
-            this.stopSnorring();
+    updateCurrentState() {
+        if (this.isDead()) {
+            this.currentState = 'dead';
+        } else if (this.isHurt()) {
+            this.currentState = 'hurt';
+        } else if (this.isAboveGround()) {
+            this.currentState = this.falling ? 'falling' : 'jumping';
+        } else if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+            this.currentState = 'walking';
+        } else {
+            this.currentState = this.getIdleState();
+        }
+    }
+
+    /**
+     * Determines idle state based on time since last input
+     */
+    getIdleState() {
+        const timeSinceLastInput = new Date().getTime() - this.lastKeyboardHit;
+        return timeSinceLastInput > 5500 ? 'longIdle' : 'idle';
+    }
+
+    /**
+     * Plays current animation based on state
+     */
+    playCurrentAnimation() {
+        const config = this.animationConfig[this.currentState];
+        const images = this.animations[this.currentState];
+
+        if (!config || !images) return;
+
+        if (config.frameSkip && this.frameCounter % config.frameSkip !== 0) return;
+
+        if (config.sound) {
+            this.playConfiguredSound(config.sound);
+        }
+
+        if (config.type === 'once') {
+            this.playAnimationOnce(images);
+            if (config.callback) config.callback();
+        } else {
+            this.playAnimation(images);
+        }
+    }
+
+    /**
+     * Plays configured sound
+     */
+    playConfiguredSound(soundKey) {
+        const sound = this[soundKey + '_sound'];
+        if (sound) {
+            if (soundKey === 'snoring' && sound.paused) {
+                this.playSound(sound, soundVolume);
+            } else if (soundKey !== 'snoring') {
+                this.playSound(sound, soundVolume);
+            }
         }
     }
 
     /**
      * Stops snoring sound
-     * 
-     * @returns {void}
      */
-    stopSnorring() {
-        if (this.snorring_sound) {
-            this.snorring_sound.pause();
-            this.snorring_sound.currentTime = 0;
+    stopSnoring() {
+        if (this.snoring_sound) {
+            this.snoring_sound.pause();
+            this.snoring_sound.currentTime = 0;
         }
     }
 
     /**
-     * Selects appropriate animation based on character state
-     * 
-     * @returns {void}
+     * Stops all intervals
      */
-    selectAnimation() {
-        if (this.isDead()) {
-            this.handleDeadAnimation();
-        } else if (this.isHurt()) {
-            this.handleHurtAnimation();
-        } else if (this.isAboveGround()) {
-            this.handleJumpFallAnimation();
-        } else {
-            this.handleGroundAnimation();
-        }
-    }
-
-    /**
-     * Handles death animation and game over
-     * 
-     * @returns {void}
-     */
-    handleDeadAnimation() {
-        this.playAnimationOnce(this.images_dead);
-        clearInterval(this.movingInterval);
-        this.playSound(this.die_sound, soundVolume, this.animationInterval);
-
-        setTimeout(() => {
-            youLose();
-        }, 1000);
-    }
-
-    /**
-     * Handles hurt animation and sound
-     * 
-     * @returns {void}
-     */
-    handleHurtAnimation() {
-        this.playAnimation(this.images_hurt);
-        this.playSound(this.hurt_sound, soundVolume);
-        this.lastKeyboardHit = new Date().getTime();
-        this.stopSnorring();
-    }
-
-    /**
-     * Handles jump and fall animations
-     * 
-     * @returns {void}
-     */
-    handleJumpFallAnimation() {
-        if (this.falling) {
-            this.currentImageOnce = 0;
-            this.fallAnimation();
-        } else {
-            this.currentImageOnce = 0;
-            this.jumpAnimation();
-        }
-    }
-
-    /**
-     * Handles ground-based animations
-     * 
-     * @returns {void}
-     */
-    handleGroundAnimation() {
-        if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-            this.playAnimation(this.images_walking);
-        } else {
-            this.handleIdleStates();
-        }
-    }
-
-    /**
-     * Manages different idle states based on time
-     * 
-     * @returns {void}
-     */
-    handleIdleStates() {
-        let timeSinceLastInput = new Date().getTime() - this.lastKeyboardHit;
-
-        if (timeSinceLastInput > 5500) {
-            this.handleLongIdle();
-        } else if (timeSinceLastInput > 500) {
-            this.handleNormalIdle();
-        } else {
-            this.handleStillState();
-        }
-    }
-
-    /**
-     * Handles long idle animation with snoring
-     * 
-     * @returns {void}
-     */
-    handleLongIdle() {
-        if (this.frameCounter % 4 === 0) {
-            this.playAnimation(this.images_longidle);
-        }
-        if (this.snorring_sound && this.snorring_sound.paused) {
-            this.playSound(this.snorring_sound, soundVolume);
-        }
-    }
-
-    /**
-     * Handles normal idle animation
-     * 
-     * @returns {void}
-     */
-    handleNormalIdle() {
-        if (this.frameCounter % 3 === 0) {
-            this.playAnimation(this.images_idle);
-        }
-    }
-
-    /**
-     * Handles still state animation
-     * 
-     * @returns {void}
-     */
-    handleStillState() {
-        this.loadImage(this.images_idle[0]);
-    }
-
-    /**
-     * Handles jump animation sequence
-     * 
-     * @returns {void}
-     */
-    jumpAnimation() {
-        if (isPaused) return;
-        clearInterval(this.animationInterval)
-        this.jumpInterval = setInterval(() => {
-            if (!this.falling) {
-                this.playAnimationOnce(this.images_jumping)
-            } else {
-                clearInterval(this.jumpInterval)
-                this.animation()
-            }
-        }, 50);
-    }
-
-    /**
-     * Handles fall animation sequence
-     * 
-     * @returns {void}
-     */
-    fallAnimation() {
-        if (isPaused) return;
-        clearInterval(this.animationInterval)
-        this.fallInterval = setInterval(() => {
-            if (this.falling) {
-                this.playAnimationOnce(this.images_falling)
-            } else {
-                clearInterval(this.fallInterval)
-                this.animation()
-            }
-        }, 50);
+    stopAllIntervals() {
+        Object.values(this.intervals).forEach(interval => {
+            if (interval) clearInterval(interval);
+        });
+        this.intervals = {};
+        this.stopSnoring();
     }
 }
